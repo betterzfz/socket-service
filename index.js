16173cbf0d1905be489ed03654034fdb7b079104
betterzfz <betterzfz@163.com>
2016-12-18 22:13:28 +0800

    adhibit express

--- index.js
+++ index.js
@@ -1,28 +1,21 @@
-const http = require('http');
-const app = http.createServer((req, res) => {
-    console.log('serving...');
-    res.end('serving...');
-});
-const io = require('socket.io')(app);
+const app = require('express')();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
 
 const hostname = '127.0.0.1';
 const port = 3000;
-app.listen(port, hostname, () => {
+server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
 
-io.on('connection', socket => {
-    socket.on('add', data => {
-        console.log(data);
-        socket.emit('add', data);
-    });
+app.get('/', (req, res) => {
+    console.log('serving...');
+    res.send('serving...');
 });
 
-http.get({
-    hostname: hostname,
-    port: port,
-    path: '/library/socket_service',
-    agent: false
-}, res => {
-    console.log('ok');
+io.on('connection', socket => {
+    socket.on('register_action', data => {
+        console.log(data);
+        socket.emit(data.action, data.data);
+    });
 });
