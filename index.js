8078d623b36f6964735c1053606ed12161d08c8b
betterzfz <betterzfz@163.com>
2016-12-19 19:10:08 +0800

    add config for the service

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+const config = require('config');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+const hostname = config.get('hostname');
+const port = config.get('port');
 server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
@@ -13,9 +15,10 @@ app.get('/', (req, res) => {
     res.send('serving...');
 });
 
+app.use(express.static('public'))
+
 io.on('connection', socket => {
     socket.on('register_action', data => {
-        console.log(data);
-        socket.emit(data.action, data.data);
+        io.emit(data.action, data.data);
     });
 });
--- public/socket-service.js
+++ public/socket-service.js
@@ -1,1 +1,4 @@
-console.log('ok');
+$.getScript('http://127.0.0.1:3000/socket.io/socket.io.js', () => {
+    console.log('ok');
+    console.log(io);
+});
