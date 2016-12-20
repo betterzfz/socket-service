4f29969c99b8b0195ad781c99b833e70579190ef
betterzfz <betterzfz@163.com>
2016-12-20 21:50:32 +0800

    add class SocketService

--- public/socket-service.js
+++ public/socket-service.js
@@ -1,4 +1,13 @@
-$.getScript('http://127.0.0.1:3000/socket.io/socket.io.js', () => {
-    console.log('ok');
-    console.log(io);
-});
+class SocketService {
+    constructor (hostname, port) {
+        this.socket = io.connect(`http://${hostname}:${port}`);
+    }
+    register (action, data) {
+        this.socket.emit('register_action', { action : action, data : data });
+    }
+    apply (action, callback) {
+        this.socket.on(action, data => {
+            callback(data);
+        });
+    }
+}
