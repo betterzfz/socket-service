add03231a328cdf67718ef893214715e867b621b
betterzfz <betterzfz@163.com>
2016-12-27 18:32:42 +0800

    add socket service module

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const config = require('config');
+const circular_json = require('circular-json');
 
 const hostname = config.get('hostname');
 const port = config.get('port');
@@ -13,6 +14,13 @@ server.listen(port, hostname, () => {
 app.get('/', (req, res) => {
     console.log('serving...');
     res.send('serving...');
+});
+
+app.get('/socket-service', (req, res) => {
+    const SocketService = require('socket-service');
+    socket_service = new SocketService(hostname, port);
+    console.log(req);
+    res.send(circular_json.stringify({socket_service : socket_service}));
 });
 
 app.use(express.static('public'))
