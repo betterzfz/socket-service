a3b4563c988e6c7cdf7214d97682787d455d43c7
betterzfz <betterzfz@163.com>
2016-12-28 11:05:23 +0800

    send file when request socket service

--- index.js
+++ index.js
@@ -18,9 +18,33 @@ app.get('/', (req, res) => {
 
 app.get('/socket-service', (req, res) => {
     const SocketService = require('socket-service');
+    const fs = require('fs');
     socket_service = new SocketService(hostname, port);
-    console.log(req);
-    res.send(circular_json.stringify({socket_service : socket_service}));
+    for (i in socket_service) {
+        console.log(i);
+    }
+    fs.writeFile(__dirname + '/public/serve-socket.js', 'const socket_service = ' + circular_json.stringify(socket_service), err => {
+        if (err) {
+            throw err;
+        }
+        res.sendFile('serve-socket.js', {
+            root: __dirname + '/public/',
+            dotfiles: 'deny',
+            header: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        }, err => {
+            if (err) {
+                console.log(err);
+                res.status(err.status).end();
+            } else {
+                console.log('Sent:socket-service.json');
+            }
+        });
+    });
+    // console.log(req);
+    // res.send();
 });
 
 app.use(express.static('public'))
